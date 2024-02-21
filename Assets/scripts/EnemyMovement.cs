c89074fa5c544005cddcf53a6cc1ee0bb903f495
Oceanevg <148327099+Oceanevg@users.noreply.github.com>
2024-02-21 10:32:08 +0100

    nouveau projet

---
+++ Assets/scripts/EnemyMovement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed;
+    public Transform[] waypoints;
+
+    private Transform target;
+    private int destPoint = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = waypoints[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        // Si l'ennmi est quasiment arrivé à destination
+        if(Vector3.Distance(transform.position, target.position) < 0.3f )
+        {
+            destPoint = (destPoint + 1) % waypoints.Length;
+            target = waypoints[destPoint];
+        }
+    }
+}
